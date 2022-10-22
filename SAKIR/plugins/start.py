a7423e6ded0805a5598bbf189709acbd748212fc
SakirBey1 <sakirhack81@gmail.com>
2022-10-23 02:37:50 +0300

    ``fix``
    ``fix``

---
+++ SAKIR/plugins/start.py
@@ -0,0 +1,17 @@
+from .. import worker
+from telethon import events, Button
+
+@worker.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply("ѕοмє ιиƒο αϐουτ οωиєя.",
+                    buttons=[
+                        [Button.url("οωиєя", url="https://xnkit.github.io/k")],
+                        [Button.inline("Cнєϲκ мє",data="op")]
+                    ])
+
+@worker.on(events.callbackquery.CallbackQuery(data="op"))
+async def ex(event):
+    await event.edit("υѕє τнιѕ ρℓєαѕє /help",
+                    buttons=[
+                        [Button.url("οωиєя", url="https://xnkit.github.io/k")]
+                    ])
