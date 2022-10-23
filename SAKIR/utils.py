6873c466dc2d3e0d71c0c2e4c9708309445066d0
SakirBey1 <sakirhack81@gmail.com>
2022-10-23 03:10:25 +0300

    Update utils.py

--- SAKIR/utils.py
+++ SAKIR/utils.py
@@ -1,19 +1,18 @@
 # Developer By : @SakirBey1
 # Geliştirici : @SakirBey1
 
-import sys 
+import sys
 import logging
 import importlib
 from telethon import TelegramClient, events
 from pathlib import Path
 
 def load_plugins(plugin_name):
-  path = Path(f"SAKIR/plugins/{plugin_name}.py")
-  name = "SAKIR.plugins.{}".format(plugin_name)
-  spec = importlib.util.spec_from_file_location(name, path)
-  load = importlib.util.module_from_spec(spec)
-  load.logger = logging.getLogger(plugin_name)
-  spec.loader.exec_module(load)
-  sys.modules["SAKIR.plugins." + plugin_name] = load
-  print("𝕤𝕡𝕒𝕞 𝕘ö𝕟𝕕𝕖𝕣𝕚𝕔𝕚 𝕓𝕠𝕥 𝕚ç𝕖 𝕒𝕜𝕥𝕒𝕣𝕕ı" + plugin_name)
-
+    path = Path(f"SAKIR/plugins/{plugin_name}.py")
+    name = "SAKIR.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["SAKIR.plugins." + plugin_name] = load
+    print("sᴘᴀᴍᴍᴇʀ ʙᴏᴛ ʜᴀs ɪᴍᴘᴏʀᴛᴇᴅ " + plugin_name)
