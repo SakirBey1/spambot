2b1be94498f653869ca74e7195f7310f06a7e4e4
SakirBey1 <sakirhack81@gmail.com>
2022-10-23 03:01:37 +0300

    ``fix``
    ``fix``

--- SAKIR/utils.py
+++ SAKIR/utils.py
@@ -12,7 +12,7 @@ def load_plugins(plugin_name):
   name = "SAKIR.plugins.{}".format(plugin_name)
   spec = importlib.util.spec_from_file_location(name, path)
   load = importlib.util.module_from_spec(spec)
-  load.logger = loggin.getLogger(plugin_name)
+  load.logger = logging.getLogger(plugin_name)
   spec.loader.exec_module(load)
   sys.modules["SAKIR.plugins." + plugin_name] = load
   print("𝕤𝕡𝕒𝕞 𝕘ö𝕟𝕕𝕖𝕣𝕚𝕔𝕚 𝕓𝕠𝕥 𝕚ç𝕖 𝕒𝕜𝕥𝕒𝕣𝕕ı" + plugin_name)
